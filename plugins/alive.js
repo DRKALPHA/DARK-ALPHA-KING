ffcaed18b3bc68acf7fa23953ffe21bbd07cc941
DRK DEVELOPER <146550095+DRKALPHA@users.noreply.github.com>
2024-11-17 14:07:28 +0530

    Add files via upload

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,5 +1,7 @@
-const {readEnv} = require('../lib/database')
+const config = require('../config')
 const {cmd , commands} = require('../command')
+const os = require('os')
+const {runtime} = require('../lib/functions')
 
 cmd({
     pattern: "alive",
@@ -9,8 +11,36 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-const config = await readEnv();
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
+
+let desc = `
+*Hi 🍄* ${pushname}
+
+┌────────────────
+│❖ *ᴜᴘᴛɪᴍᴇ :* _${runtime(process.uptime())}_
+│❖ *ʀᴀᴍ ᴜꜱᴀɢᴇ :*  _${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB_
+│❖ *ʜᴏꜱᴛ ɴᴀᴍᴇ :* _${os.hostname()}_
+│❖ *ᴏᴡɴᴇʀ :* _Sadeesha Tharumin_
+└────────────────
+
+*I'm Alive Now 👸🏻*
+
+┌────────────────
+│ _*Alexa Official Web Site*_
+│ https://sadiyamin.github.io/|||Test
+└────────────────
+┌────────────────
+│ _*Alexa Github Repo*_
+│ http://github.com/sadiyamin/Alexa 
+└────────────────
+┌────────────────
+│ _*Alexa Public Group*_
+│ https://chat.whatsapp.com/Ci5mDk9zEVF95NcuqEtzl4
+└────────────────
+
+> *©ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴇᴇꜱʜᴀ ᴄᴏᴅᴇʀ  🧑🏻‍💻*
+`
+return await conn.sendMessage(from,{image: {url: `https://i.ibb.co/z52R6XR/bg.jpg`},caption: desc},{quoted: mek})
+
 }catch(e){
 console.log(e)
 reply(`${e}`)
